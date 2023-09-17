2ddc8bfddcae55882430a873645f14772c494154
ThaisRozas <thaisrozas@gmail.com>
2023-09-17 19:58:45 -0300

    Regressão com testes dos modelos e início de classificação

--- regression-model.py
+++ regression-model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plotarGraficos = False
+plotarGraficos = True
 
 #COLETA DE DADOS:
 Data = np.loadtxt("sorvete.csv",delimiter=',', skiprows=1)
@@ -20,7 +20,7 @@ ax.scatter(X, y, color='red',edgecolors='k')
 # ORGANIZAR OS DADOS
 
 N = X.shape[0]
-print(N)
+#print(N)
 X = X.reshape(N, 1)
 y = y.reshape(N, 1)
 
@@ -36,8 +36,7 @@ MSE_MEDIA =[]
 
 # DEFININDO RODADAS
 
-#R=1000
-R = 1
+R = 1000
 
 for r in range(R):
     
@@ -60,11 +59,7 @@ for r in range(R):
 
     #Treinamento dos modelos  
     b_media = np.mean(y_treino)
-    b_media = np.array([
-        [b_media],
-        [0],
-        [0]
-    ])
+    b_media = np.array([[b_media],[0]])
 
 
     b_OLS_S = np.linalg.pinv(X_treino.T@X_treino)@X_treino.T@y_treino #modelo
@@ -75,37 +70,46 @@ for r in range(R):
     X_treino = np.concatenate((ones,X_treino),axis=1)
     b_OLS_C = np.linalg.pinv(X_treino.T@X_treino)@X_treino.T@y_treino #modelo com interceptor
 
-    #posso colocar um contador pra ir até 1000, plotando todos os gráficos
+    if plotarGraficos:
 
-    #if plotarGraficos:
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.scatter(X_treino[:,1],y_treino[:,0],color='purple',edgecolors='k')
-    plt.show()
-    
-    
-    
-    x_axis = np.linspace(0,40,500)
-    y_axis = np.linspace(0,750,500)
-    plt.show()
-    #Consigo printar o treinamento
-    
+        '''
+            PARA CONSTRUIR O GRÁFICO DE CADA ITERAÇÃO
+            fig = plt.figure()
+            ax = fig.add_subplot()
+            ax.scatter(X_treino[:,1],y_treino[:,0],color='purple',edgecolors='k')
+            plt.show()
+        '''
+        
+        x_axis = np.linspace(0,40,500)
+        y_axis = np.linspace(0,750,500)
 
-    '''
-    PARTE QUE NÃO ESTÁ FUNCIONANDO!
-    A DIMENSÃO DAS MATRIZES QUE SE MULTIPLICAM TÁ ERRADA
+        X_map,Y_map = np.meshgrid(x_axis,y_axis)
+        X_map.shape = (500,500,1)
+        Y_map.shape = (500,500,1)
+        ones_map = np.ones((500,500,1))
 
-    X_map,Y_map = np.meshgrid(x_axis,y_axis)
-    X_map.shape = (500,500,1)
-    Y_map.shape = (500,500,1)
-    ones_map = np.ones((500,500,1))
-    X3D = np.concatenate((ones_map,X_map,Y_map),axis=1)
-    Z_media = X3D@b_media
-    Z_ols_s = X3D@b_OLS_S
-    Z_ols_c = X3D@b_OLS_C
+        X3D = np.concatenate((ones_map,X_map,Y_map),axis=1)
 
-    ax.plot_surface(X_map[:,:,0],Y_map[:,:,0],Z_media[:,:,0],cmap='gray',alpha=.5)
-    ax.plot_surface(X_map[:,:,0],Y_map[:,:,0],Z_ols_s[:,:,0],cmap='winter',alpha=.5)
-    ax.plot_surface(X_map[:,:,0],Y_map[:,:,0],Z_ols_c[:,:,0],cmap='jet',alpha=.5)
+        Z_media = X3D@b_media.T
+        Z_ols_s = X3D@b_OLS_S.T
+        Z_ols_c = X3D@b_OLS_C.T
+        
+
+    #Teste dos modelos, produzindo medida de erro/acerto 
+    ones = np.ones((X_teste.shape[0],1))
+    X_teste = np.concatenate((ones,X_teste),axis=1)
     
-    plt.show()'''
+    y_pred_media = X_teste@b_media
+    y_pred_ols_c = X_teste@b_OLS_C
+    y_pred_ols_s = X_teste@b_OLS_S
+
+
+    MSE_MEDIA.append(np.mean((y_teste-y_pred_media)**2))
+    MSE_OLS_C.append(np.mean((y_teste-y_pred_ols_c)**2))
+    MSE_OLS_S.append(np.mean((y_teste-y_pred_ols_s)**2))
+
+    bp=1
+
+boxplot = [MSE_MEDIA,MSE_OLS_C,MSE_OLS_S]
+plt.boxplot(boxplot,labels=['Média','OLS com i','OLS sem i'])
+plt.show()
